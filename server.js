f0c5d1ab2d6ac6a82ad30e45b183396cb42a5956
oded <odahari@gmail.com>
2018-05-24 01:03:03 +0300

    hearku port and start script

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -19,7 +20,7 @@ app.use(express.static(__dirname + '/public'));
 //         }
 //     });
 //     next();
-// });
+// });enc
 
 app.use((req, res, next) => {
     var now = new Date().toString();
@@ -41,7 +42,7 @@ app.use((req, res, next) => {
      ///////////////////
     //////////////////////
     /////////////////////
-    / ADDING VERSION CONTROL
+    // ADDING VERSION CONTROL
     ///////////////////
     //////////////////////
     /////////////////////
@@ -77,6 +78,6 @@ app.get('/bad', (req, res) => {
     res.send({ errorMessage: 'bad bad bad guitar' });
 
 });
-app.listen(3000, () => {
-    console.log('server is up');
+app.listen(port, () => {
+    console.log(`server is up on port ${port}`);
 });
